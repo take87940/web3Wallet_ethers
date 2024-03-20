68ac5a522a1683cecd06c9675da0510392c46c30
take87940 <130485621+take87940@users.noreply.github.com>
2024-03-21 00:08:28 +0800

    ffirst commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {ethers} from "ethers"
+import { useState, useEffect } from "react"
 
-function App() {
+const App = () =>
+{
+  const [balance, setBalance] = useState();
+  const [currentAccount, setCurrentAccount] = useState();
+
+  useEffect(()=>{
+    if(!currentAccount || !ethers.isAddress(currentAccount)) return;
+
+    if(!window.ethereum) return;
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    provider.getBalance(currentAccount).then((result) => {
+      setBalance(ethers.formatEther(result));
+    });
+    provider.getNetwork().then((result) => {
+      console.log(result);
+    })
+  }, [currentAccount])
+
+  window.ethereum.on('accountsChanged', async function(accounts)
+  {
+    setCurrentAccount(accounts[0]);
+  })
+
+  const onClickConnect = () =>{
+    if(!window.ethereum){
+      console.log("please install metamask");
+      return;
+    }
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+
+    provider.send("eth_requestAccounts", [])
+    .then((accounts) => {
+      if(accounts.length > 0) setCurrentAccount(accounts[0])
+    })
+    .catch((e) => console.log(e));
+  }
+
+  const onClickDisConnect = () =>
+  {
+    console.log("onClickDisconnect!");
+    setBalance(undefined);
+    setCurrentAccount(undefined);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {currentAccount
+      ? <button onClick={onClickDisConnect}>
+          Account:{currentAccount}
+        </button>
+      : <button onClick={onClickConnect}>
+          Connect MetaMask
+        </button>
+      }
+      {currentAccount
+        ?<div>
+           <div>ETH Balance of current account: {balance}</div>
+         </div>
+        :<div></div>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
